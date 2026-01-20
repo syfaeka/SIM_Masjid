834437a3f7ffb1229ebe4bf5864fb7adf4e9f794
syfaeka <muhammadsyfa77@Gmail.com>
2026-01-20 10:07:41 +0700

    jjk

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,13 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => 'admin123',
+        $this->call([
+            UserSeeder::class,
         ]);
     }
 }
